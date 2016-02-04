445a527822745d0aaeef848d5a81403ce7a8c998
stengerben <stengerben@gmail.com>
2016-02-04 11:59:52 -0800

    Handle unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Successful update
+    else
+      render 'edit'
+    end
+  end
   
   private
 
